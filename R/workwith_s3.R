550a6ead51ca44aa09aced4cd5f5ec7fcec5b57a
Jeffrey W Hollister <hollister.jeff@epa.gov>
2023-07-06 19:04:53 +0000

    dmap timings added

--- R/workwith_s3.R
+++ R/workwith_s3.R
@@ -6,11 +6,11 @@ tic.clearlog()
 bucket <- s3_bucket("voltrondata-labs-datasets", anonymous = TRUE, region = 'us-east-2') #bucket region
 nyc_taxi <- open_dataset(bucket$path("nyc-taxi"))
 
-tic("nyc taxi on S3 from laptop: nrow")
+tic("nyc taxi on S3 from dmap: nrow")
 nrow(nyc_taxi)
 toc(log = TRUE)
 
-tic("nyc taxi on S3 from laptop: n per year")
+tic("nyc taxi on S3 from dmap: n per year")
 years <- nyc_taxi |>
   group_by(year) |>
   summarize(n = n()) |>
@@ -18,7 +18,7 @@ years <- nyc_taxi |>
 years
 toc(log = TRUE)
 
-sink("timings.txt", append = TRUE)
+sink("dmap_timings.txt", append = TRUE)
 writeLines(unlist(tic.log()))
 sink()
 
